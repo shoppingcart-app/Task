416d9dbca00a2cb3c143736dca68f3d446553c25
shoppingcart-app <76087113+shoppingcart-app@users.noreply.github.com>
2020-12-31 14:54:08 +0530

    nnnn

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -11,6 +11,49 @@ router.route('/products').post((req, res, next) => {
       res.json(data)
     }
   });
-}).get
+}).get((req, res, next) => {
+  productController.getAllProduct(req.body, (error, data) => {
+    if (error) {
+      res.status(500).send(error)
+    } else {
+      console.log(data)
+      res.json(data)
+    }
+  });
+})
+
+router.route('/products/:id').get((req, res, next) => {
+  productController.getOneProduct(req.body, (error, data) => {
+    if (error) {
+      res.status(500).send(error)
+    } else {
+      console.log(data)
+      res.json(data)
+    }
+  });
+})
+
+router.route('/products/:id').put((req, res, next) => {
+  productController.updateProduct(req.body, (error, data) => {
+    if (error) {
+      res.status(500).send(error)
+    } else {
+      console.log(data)
+      res.json(data)
+    }
+  });
+})
+
+router.route('/products/:id').delete((req, res, next) => {
+  productController.deleteProduct(req.body, (error, data) => {
+    if (error) {
+      res.status(500).send(error)
+    } else {
+      console.log(data)
+      res.json(data)
+    }
+  });
+})
+
 
 module.exports = router;
