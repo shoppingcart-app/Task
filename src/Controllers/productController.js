84977bdb3cf73f07e4f2b91d584cdd6c1d19260a
shoppingcart-app <76087113+shoppingcart-app@users.noreply.github.com>
2021-01-27 10:17:06 +0530

    Update productController.js

--- src/Controllers/productController.js
+++ src/Controllers/productController.js
@@ -1,73 +1,62 @@
+const { async } = require('asyncawait');
 const productSchema = require('../Models/Product');
-
+ 
 //POST
-const createProduct = (product) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const Product = new productSchema(product);
-      console.log(product);
-      await Product.save(function(err,result){ 
-        if (err){ 
-          console(err);
-          reject(err);
-        } 
-        else{ 
-          console.log(result);
-          resolve(result);
-        } 
+const createProduct = (product)  =>{
+  return new Promise(async(resolve,reject)=>{
+    const Product = new productSchema(product);
+    await Product.save(function (error,data) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(data);
+      }
     })
-      
-    } catch (err) {
-      console.log(err)
-      reject(err);
-    }
-  })
+})
 }
-
+ 
 //GET ALL
-const getAllProduct = (product, callback) => {
+const getAllProduct = (product ,callback)  =>{
   productSchema.find(product, (error, data) => {
     if (error) {
       return callback(error)
     } else {
-      return callback(null, data)
+      return callback(null,data)
     }
   })
 }
-
+ 
 //GET One
-const getOneProduct = (product, callback) => {
+const getOneProduct = (product ,callback)  =>{
   productSchema.findById(product, (error, data) => {
     if (error) {
       return callback(error)
     } else {
-      return callback(null, data)
+      return callback(null,data)
     }
   })
 }
-
-const updateProduct = (product, callback) => {
+ 
+const updateProduct = (product ,callback)  =>{
   productSchema.findByIdAndUpdate(product, (error, data) => {
     if (error) {
       return callback(error)
     } else {
-      return callback(null, data)
+      return callback(null,data)
     }
   })
 }
-
-const deleteProduct = (product, callback) => {
+ 
+const deleteProduct = (product ,callback)  =>{
   productSchema.findByIdAndRemove(product, (error, data) => {
     if (error) {
       return callback(error)
     } else {
-      return callback(null, data)
+      return callback(null,data)
     }
   })
 }
-
-module.exports = {
-  createProduct, getAllProduct, getOneProduct, getAllProduct, updateProduct, deleteProduct
-
+ 
+module.exports = { createProduct,getAllProduct,getOneProduct,getAllProduct,updateProduct,deleteProduct
+ 
 }
-
